fe28d4f5e3fa7bf0484171f08a296d280a178568
Gaëtan <gaetan@moveo-webdesign.com>
2021-05-04 12:28:57 +0200

    feat: refactor filemanager - move files as props for file manager component

--- src/scripts/components/FileManager.tsx
+++ src/scripts/components/FileManager.tsx
@@ -8,29 +8,6 @@ import {IFile} from "../interfaces";
 import {StoreProvider} from "easy-peasy";
 import {createFileManagerStore} from "../store/filemanager.store";
 
-const files: Array<IFile> = [
-    {url: "https://picsum.photos/id/350/200/300", width: 250},
-    {url: "https://picsum.photos/id/351/250/300", width: 250},
-    {url: "https://picsum.photos/id/352/300/200", width: 250},
-    {url: "https://picsum.photos/id/353/300/300", width: 250},
-    {url: "https://picsum.photos/id/353/300/300", width: 250},
-    {url: "https://picsum.photos/id/353/300/300", width: 250},
-    {url: "https://picsum.photos/id/353/300/300", width: 250},
-    {url: "https://picsum.photos/id/353/300/300", width: 250},
-    {url: "https://picsum.photos/id/353/300/300", width: 250},
-    {url: "https://picsum.photos/id/353/300/150", width: 250},
-    {url: "https://picsum.photos/id/353/300/150", width: 250},
-    {url: "https://picsum.photos/id/353/300/150", width: 250},
-    {url: "https://picsum.photos/id/353/300/150", width: 250},
-    {url: "https://picsum.photos/id/353/300/150", width: 250},
-    {url: "https://picsum.photos/id/353/300/150", width: 250},
-    {url: "https://picsum.photos/id/353/300/150", width: 250},
-    {url: "https://picsum.photos/id/353/300/150", width: 250},
-    {url: "https://picsum.photos/id/353/300/150", width: 250},
-    {url: "https://picsum.photos/id/353/300/150", width: 250},
-    {url: "https://picsum.photos/id/354/200/300", width: 250}
-]
-
 
 function Image({file}: { file?: IFile }) {
     return (
@@ -42,7 +19,11 @@ function Image({file}: { file?: IFile }) {
     )
 }
 
-function FileManager() {
+interface FileManagerProps<T>{
+    files: Array<T>
+}
+
+function FileManager({files}:FileManagerProps<IFile>) {
 
     const store = createFileManagerStore<IFile>(files);
 
--- src/scripts/main.tsx
+++ src/scripts/main.tsx
@@ -2,11 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './../styles/index.scss';
 import FileManager from "./components/FileManager";
+import {IFile} from "./interfaces";
+
+const files: Array<IFile> = [
+    {url: "https://picsum.photos/id/350/200/300", width: 250},
+    {url: "https://picsum.photos/id/351/250/300", width: 250},
+    {url: "https://picsum.photos/id/352/300/200", width: 250},
+    {url: "https://picsum.photos/id/353/300/300", width: 250},
+    {url: "https://picsum.photos/id/353/300/300", width: 250},
+    {url: "https://picsum.photos/id/353/300/300", width: 250},
+    {url: "https://picsum.photos/id/353/300/300", width: 250},
+    {url: "https://picsum.photos/id/353/300/300", width: 250},
+    {url: "https://picsum.photos/id/353/300/300", width: 250},
+    {url: "https://picsum.photos/id/353/300/150", width: 250},
+    {url: "https://picsum.photos/id/353/300/150", width: 250},
+    {url: "https://picsum.photos/id/353/300/150", width: 250},
+    {url: "https://picsum.photos/id/353/300/150", width: 250},
+    {url: "https://picsum.photos/id/353/300/150", width: 250},
+    {url: "https://picsum.photos/id/353/300/150", width: 250},
+    {url: "https://picsum.photos/id/353/300/150", width: 250},
+    {url: "https://picsum.photos/id/353/300/150", width: 250},
+    {url: "https://picsum.photos/id/353/300/150", width: 250},
+    {url: "https://picsum.photos/id/353/300/150", width: 250},
+    {url: "https://picsum.photos/id/354/200/300", width: 250}
+]
 
 function Main() {
     return (
         <div>
-            <FileManager/>
+            <FileManager files={files}/>
         </div>
     )
 }
