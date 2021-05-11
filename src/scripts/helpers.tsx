9545b49165254b68d88c63b8e41a633714bcdd44
Gaëtan <gaetan@moveo-webdesign.com>
2021-05-11 22:38:15 +0200

    fix: #file-manager css rules

--- src/scripts/helpers.tsx
+++ src/scripts/helpers.tsx
@@ -1,12 +1,3 @@
-import {useEffect, useState} from "react";
-
-export function useStateWithCallback<T>(initialState: T, callback: Function): [T, Function] {
-    const [state, setState] = useState<T>(initialState);
-    useEffect(() => callback(state), [state]);
-
-    return [state, setState];
-}
-
 export function arrayHas<T>(collection: Array<T>, item: T): boolean {
     return collection.some((entry: T) => item === entry);
 }
